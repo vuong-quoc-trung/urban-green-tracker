39a876c1dae3baa25b07ac8051c1f48da753cb29
ASUS <102240173@sv1.dut.udn.vn>
2026-07-09 11:06:28 +0700

    Tạo form backend cơ bản

---
+++ backend/urban-green-tracker/src/main/java/com/urbangreen/urban_green_tracker/UrbanGreenTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.urbangreen.urban_green_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrbanGreenTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UrbanGreenTrackerApplication.class, args);
+	}
+
+}
---
+++ backend/urban-green-tracker/src/test/java/com/urbangreen/urban_green_tracker/UrbanGreenTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.urbangreen.urban_green_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UrbanGreenTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
